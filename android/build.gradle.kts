8577ab900f46775c31cab84846d4a5404d2023ab
“sewerganger” <wanghan9423@outlook.com>
2021-09-02 22:37:51 +0800

    save

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,22 +1,38 @@
 plugins {
   id("com.android.application")
-//  id("$rootDir/")
 }
+
+import com . android . build . OutputFile
+
+var react: Any by ext
+
+react = mapOf("root" to "../")
 
 apply {
-  println(rootDir)
-//  from(file("$rootDir/third_party/react-native/react.gradle"))
+  //  config react-native
+  from(file("$rootDir/scripts/gradle/react-native.gradle"))
 }
-
 
 android {
   defaultConfig {
     buildToolsVersion = "30.0.2"
     applicationId("com.deskbtm.nawb")
     minSdkVersion(21)
-
+    compileSdkVersion(30)
     targetSdkVersion(targetSdkVersion = 30)
     versionCode(1)
     versionName("1.0")
   }
+
+  splits {
+    abi {
+      reset()
+      isEnable = true
+      isUniversalApk = false
+      include("armeabi-v7a", "x86", "arm64-v8a", "x86_64")
+    }
+  }
+  
+  
+  
 }
