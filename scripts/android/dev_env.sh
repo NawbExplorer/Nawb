420b535a0079f3a1fecb6ddbfdfeb7c30119a4a3
Nawbc <wanghan9423@outlook.com>
2021-12-01 12:28:48 +0800

    save

---
+++ scripts/android/dev_env.sh
@@ -0,0 +1,18 @@
+for e in node yarn npm pnpm
+do
+    ln -s "$(which $e)" /usr/bin/$e
+done
+
+
+echo "
+NDK_VERSION=21.4.7075529
+export ANDROID_HOME=~/Android/Sdk
+export ANDROID_SDK_ROOT=\$ANDROID_HOME
+export ANDROID_NDK=\$ANDROID_HOME/ndk/\$NDK_VERSION
+export JAVA_HOME=/usr/lib/jvm/default
+export PATH=\$ANDROID_NDK:\$ANDROID_HOME/cmdline-tools/latest/bin:\$ANDROID_HOME/emulator:\$ANDROID_HOME/platform-tools:\$ANDROID_HOME/tools:\$ANDROID_HOME/tools/bin:/opt/buck/bin/:\$PATH
+" >> /etc/profile
+
+source /etc/profile
+
+
--- scripts/android/dev_env_vars.sh
+++
@@ -1,8 +0,0 @@
-NDK_VERSION=21.4.7075529
-
-export ANDROID_HOME=~/Android/Sdk
-export ANDROID_SDK_ROOT=$ANDROID_HOME
-export ANDROID_NDK=$ANDROID_HOME/ndk/$NDK_VERSION
-export JAVA_HOME=/usr/lib/jvm/default
-
-export PATH=$ANDROID_NDK:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:/opt/buck/bin/:$PATH
