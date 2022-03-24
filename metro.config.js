a41c1f00daa281d97960aedd05f18a1b33f60b22
Nawbc <wanghan9423@outlook.com>
2022-03-24 15:24:41 +0800

    save

--- metro.config.js
+++ metro.config.js
@@ -7,6 +7,9 @@
 const path = require('path');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 
+/**
+ * @type import('@types/metro-config').ConfigT
+ */
 module.exports = {
   projectRoot: __dirname,
   transformer: {
